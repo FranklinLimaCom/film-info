6acddb4d5935ff3fe3c9cad69840e5d3bb3dea50
FranklinLimaCom <franklin.lima.tsi@gmail.com>
2022-10-25 19:06:11 -0300

    feat: create project

---
+++ pages/components/SearchBar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { TextField } from "@mui/material";
+
+function SearchBar() {
+  <TextField id="outlined-basic" variant="outlined" fullWidth label="Search" />;
+}
+
+export default SearchBar;
